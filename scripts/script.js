dd0ca7934db9cb62e77cd8127f423e76d19d19d0
Ian-kowalski <ian.kowalski@hotmail.com>
2024-02-20 16:30:54 +0100

    contact form

--- scripts/script.js
+++ scripts/script.js
@@ -1,9 +1,7 @@
-
-
-  fetch("nav.html")
+fetch("nav.html")
   .then(response => {
     return response.text()
   })
   .then(data => {
     document.querySelector("nav").innerHTML = data;
-  });
+  }); 
